07496ed14eb8882e10c4c5cc7c059b206f4fe91a
Galaxyjie <75266632@qq.com>
2022-11-04 13:07:18 +0800

    fix 6

--- task11.5/count_6.v
+++ task11.5/count_6.v
@@ -2,7 +2,7 @@ module count_6(
     input wire rst,
     input wire clk,
     input wire en,
-    output reg [3:0] count,
+    output reg [2:0] count,
     output reg co
 );
     always @ (posedge clk) begin
