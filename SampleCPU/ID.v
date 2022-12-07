fd16f7098ed95437eb2f25ab8fb2ec52de6dbd19
Galaxyjie <75266632@qq.com>
2022-12-07 23:30:12 +0800

    jal

--- SampleCPU/ID.v
+++ SampleCPU/ID.v
@@ -118,6 +118,7 @@ module ID(
     //新增指令
     wire inst_subu;
     wire inst_jr;
+    wire inst_jal;
 
     wire op_add, op_sub, op_slt, op_sltu;
     wire op_and, op_nor, op_or, op_xor;
@@ -152,13 +153,14 @@ module ID(
     //新增指令
     assign inst_subu    = op_d[6'b00_0000]&sa==5'b00000&func_d[6'b10_0011];
     assign inst_jr      = op_d[6'b00_0000]&inst[20:11]==10'b00000_00000&sa==5'b00000&func_d[6'b00_1000];
+    assign inst_jal     = op_d[6'b00_0011];
 
     //取操作数
     // rs to reg1
     assign sel_alu_src1[0] = inst_ori | inst_addiu | inst_subu;
 
     // pc to reg1
-    assign sel_alu_src1[1] = 1'b0;
+    assign sel_alu_src1[1] = inst_jal;
 
     // sa_zero_extend to reg1
     assign sel_alu_src1[2] = 1'b0;
@@ -171,14 +173,14 @@ module ID(
     assign sel_alu_src2[1] = inst_lui | inst_addiu;
 
     // 32'b8 to reg2
-    assign sel_alu_src2[2] = 1'b0;
+    assign sel_alu_src2[2] = inst_jal;
 
     // imm_zero_extend to reg2
     assign sel_alu_src2[3] = inst_ori;
 
 
-
-    assign op_add = inst_addiu;
+    //选操作逻辑
+    assign op_add = inst_addiu|inst_jal;;
     assign op_sub = inst_subu;
     assign op_slt = 1'b0;
     assign op_sltu = 1'b0;
@@ -206,7 +208,7 @@ module ID(
 
     //写使能信号
     // regfile store enable
-    assign rf_we = inst_ori | inst_lui | inst_addiu|inst_subu;
+    assign rf_we = inst_ori | inst_lui | inst_addiu|inst_subu|inst_jal;
 
 
     //写入到rd
@@ -215,7 +217,7 @@ module ID(
     // store in [rt] 
     assign sel_rf_dst[1] = inst_ori | inst_lui | inst_addiu;
     // store in [31]
-    assign sel_rf_dst[2] = 1'b0;
+    assign sel_rf_dst[2] = inst_jal;
 
     // sel for regfile address
     assign rf_waddr = {5{sel_rf_dst[0]}} & rd 
@@ -254,10 +256,11 @@ module ID(
     assign rs_eq_rt = (rdata1 == rdata2);
 
     //跳转信号
-    assign br_e = inst_beq & rs_eq_rt|inst_jr;
+    assign br_e = inst_beq & rs_eq_rt|inst_jr| inst_jal;
     
     //跳转地址
     assign br_addr = inst_beq ? (pc_plus_4 + {{14{inst[15]}},inst[15:0],2'b0}) : 32'b0: 
+                    inst_jr?(rdata1):
                     inst_jal?({pc_plus_4[31:28],inst[25:0],2'b0}):
                     32'b0;
     assign br_bus = {
